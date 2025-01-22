86f63b94d74927019c7f976093e9ef8887acee16
ajjiad <ajjiad.cse@gmail.com>
2025-01-22 23:54:36 +0600

    Connect to the database using the server.js file.

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+
+// connect with database (XAMPP)
+const db = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'nodejscrud'
+});
+
+// handle the error 
+db.connect((err) => {
+    // if (err) throw err; or
+    if(err){
+        throw err;
+    }
+    console.log('Connected Successfully to the database');
+});
 
 
 app.get('/', (req, res) => {
-    res.send('API is running');
+    res.send('API is running in port 5000');
 });
 
 app.listen(5000, console.log('Server running on port 5000'));

008cdf0a5323f96b81465ccffdb1e0d77c64280f
ajjiad <ajjiad.cse@gmail.com>
2025-01-23 00:33:18 +0600

    modified the server.js file and connect to the database && optimized the PORT

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
+dotenv.config();
+//initialize the connection from ./config/db.js
+const connection = require('./config/db');
 
 
 // API to get all the data from the database
@@ -8,5 +11,17 @@ app.get('/', (req, res) => {
     res.send('API is running in port 5000');
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+app.listen(process.env.PORT || 4000, (error) => {
+    //longer version
+     if (error) {
+        console.log(`Error: ${error}`);
+     }else {
+         console.log(`Server is running on port ${process.env.PORT}`);
+    }
+
+    //shorter version
+    // if(error) throw error;
+    // console.log(`Server is running on port ${process.env.PORT}`);
+    
+});
 

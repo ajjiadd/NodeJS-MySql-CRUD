265bbeecdbb3d1c0399fb15d75db6580e6880a2e
ajjiad <ajjiad.cse@gmail.com>
2025-01-23 02:19:21 +0600

    show the create.html file using API

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 //initialize the connection from ./config/db.js
 const connection = require('./config/db');
+// Serve static files (e.g., HTML, CSS, JS, images) from the public folder.
+app.use(express.static(__dirname + "/public"));
 
 
 // API to get all the data from the database
 app.get('/', (req, res) => {
-    res.send('API is running in port 5000');
+    res.redirect('/create.html'); 
 });
 
 app.listen(process.env.PORT || 4000, (error) => {
